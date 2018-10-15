9192b9a67c37495afed6f7608400cb4e56e35b66
satya9125@gmail.com <satya9125@gmail.com>
2018-10-15 13:07:29 +0530

    #444 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { NavComponent } from './component/nav/nav.component';
+
+const appRoutes : Routes = [
+	{ path: 'profile', component: ProfileComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
