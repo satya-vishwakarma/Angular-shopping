c86e362a8938f942db5a6ab56148c811b6e5cf57
satya9125@gmail.com <satya9125@gmail.com>
2018-10-15 16:44:00 +0530

    #dashboard

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+}
